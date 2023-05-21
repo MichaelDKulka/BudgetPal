88c0d484d9eedaa0db1ac2cea7811c88d488e29b
MichaelDKulka <michaelkulka28@gmail.com>
2023-05-21 11:20:25 -0400

    Initial Commit

---
+++ BudgetPal/BudgetPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetPalApp.swift
+//  BudgetPal
+//
+//  Created by Michael Kulka on 5/21/23.
+//
+
+import SwiftUI
+
+@main
+struct BudgetPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
